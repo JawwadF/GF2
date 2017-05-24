42979b9acf3f8a6189ca8ece282671bda9986950
BiancaBilovolschi <bab42@cam.ac.uk>
2017-05-24 15:27:05 +0100

    Compiled all classes with a main.cc for testing, and moved everyhing in the prod folder

---
+++ dev/bianca code/main.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "parser.h"
+
+using namespace std;
+
+int main(void) {
+	// Construct the six classes required by the innards of the logic simulator
+	names* nmz = new names();
+	network* netz = new network(nmz);
+	devices* dmz = new devices(nmz, netz);
+	monitor* mmz = new monitor(nmz, netz);
+	scanner* smz = new scanner(nmz, "file.txt");
+	parser* pmz = new parser(netz, dmz, mmz, smz);
+	pmz->readin();
+	cout << "The content of the lookup table is " << endl;
+	for (int i = 0; i < nmz->length_of_table; i++) {
+	  cout << "id "<< i << " name: ";
+	  nmz->writename(i) ;
+	  cout<<endl;
+	}
+	return 0;
+}
