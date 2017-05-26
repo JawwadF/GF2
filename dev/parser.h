6f6e7ae7a7001c3cdb29aab9ee900527a94fd10b
BiancaBilovolschi <bab42@cam.ac.uk>
2017-05-26 12:22:35 +0100

    Adding files to the master branch to the dev folder

---
+++ dev/parser.h
@@ -0,0 +1,43 @@
+#ifndef parser_h
+#define parser_h
+
+#include "names.h"
+#include "scanner.h"
+#include "network.h"
+#include "devices.h"
+#include "monitor.h"
+
+
+using namespace std;
+
+class parser {
+  network* netz; // instantiations of various classes for parser to use.
+  devices* dmz; 
+  monitor* mmz;
+  scanner* smz;
+
+  /* put other stuff that the class uses internally here */
+  /* also declare internal functions                     */
+
+
+ public:
+  bool readin ();
+    /* Reads the definition of the logic system and builds the             */
+    /* corresponding internal representation via calls to the 'Network'    */
+    /* module and the 'Devices' module.                                    */
+  void connection(void);
+  void device(void);
+  void circuit(void);
+  void monitor_(void);
+  void xor_(void);
+  void gate(void);
+  void clock(void);
+  void switch_(void);
+  void dtype(void);
+  //void monitor(void);
+  parser (network* network_mod, devices* devices_mod,
+	  monitor* monitor_mod, scanner* scanner_mod);
+    /* the constructor takes pointers to various other classes as parameters */
+};
+
+#endif /* parser_h */
